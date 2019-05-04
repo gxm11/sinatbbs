45965818a7f64fb8372e7a477b1aad656aac3744
guoxiaomi <iamguoxiaomi@sina.com>
2019-05-04 09:02:08 +0800

    replace start.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,32 @@
-load "start.rb"
+require "rubygems"
+require "sinatra"
+require "sass"
+require "./model/comment.rb"
+
+set :bind, "0.0.0.0"
+set :port, 4567
+set :server, "thin"
+
+helpers do
+  include Rack::Utils; alias_method :h, :escape_html
+end
+
+get "/style.css" do
+  content_type "text/css", :charset => "utf-8"
+  sass :style
+end
+
+get "/" do
+  @comments = Comments.order_by(Sequel.desc(:posted_date))
+  haml :index
+end
+
+put "/comment" do
+  Comments.create({
+    :name => request[:name],
+    :title => request[:title],
+    :message => request[:message],
+    :posted_date => Time.now,
+  })
+  redirect "/"
+end
